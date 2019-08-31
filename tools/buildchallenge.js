c41293b85b9026fc38d805a7587cae28a09870b4
Roy Shmueli <2903801+2roy999@users.noreply.github.com>
2019-08-31 20:06:06 +0300

    Adding feature of resources (cross-mission validation)

--- tools/buildchallenge.js
+++ tools/buildchallenge.js
@@ -11,6 +11,44 @@ function getString(key) {
     return strings[key]||key;
 }
 
+function parseObjectiveAmount(objective) {
+    if (objective.$type === 'objective-enum') {
+        var amounts = objective.option
+          .map(opt => ({ [opt.$.name]: Number(opt.$['resource-use'] || 0) }))
+          .reduce((obj, entry) => Object.assign(obj, entry), {})
+
+        return `(${JSON.stringify(amounts)}[${jsName(objective.$.id)}])`
+    } else if (objective.$type === 'objective-number') {
+        return `(${jsName(objective.$.id)} * ${objective.$['resource-use'] || 1})`
+    } else if (objective.$type === 'objective-yesno') {
+        return `((${jsName(objective.$.id)} === 'yes') ? ${objective.$['resource-use'] || 1} : 0)`
+    } else {
+        new Error('Objective don\'t support resources')
+    }
+}
+
+function parseResources(xml) {
+    return xml.resources[0].resource.map(resource => {
+        const objectives = xml.mission
+          .map(m => Object.keys(m)
+            .filter(k => k.startsWith('objective-'))
+            .map(k => m[k].map(obj => Object.assign({ $type: k }, obj)))
+            .reduce((arr, item) => arr.concat(item), [])
+          )
+          .reduce((arr, item) => arr.concat(item), [])
+          .filter(obj => obj.$.resource === resource.$.name)
+
+        return {
+            [resource.$.name]: {
+                args: objectives.map(obj => jsName(obj.$.id)),
+                code: `if (${objectives.map(parseObjectiveAmount).join(' + ')} > ${resource.$.quota}) ` +
+                  `{ return new Error('${resource.message}') }`
+            }
+        }
+    })
+      .reduce((obj, entry) => Object.assign(obj, entry), {})
+}
+
 /* parse the case element */
 function parseCondition(xml) {
     var obj = {
@@ -33,18 +71,20 @@ function parseCondition(xml) {
     return obj;
 }
 
-function parseScore(xml) {
+function parseScore(xml, resources) {
     var deps = xml.indexes[0].index.map(function(index) {
         return jsName(index.$.objective);
     });
-    var lines = xml.cases[0].case.map(parseCondition).map(function(line) {
+    var lines = resources.map(r => r.code)
+      .concat(xml.cases[0].case.map(parseCondition).map(function(line) {
         var conditions = line.values.map(function(value,index) {
             return deps[index] + ' === \'' + value + '\'';
         }).join(' && ');
 
         return 'if ('+conditions+') {return '+line.returnValue+'}';
-    }).join('\n');
-    var args = unique(deps).join(',');
+    }))
+      .join('\n');
+    var args = unique(deps.concat(...resources.map(r => r.args))).join(',');
     return 'function('+args+') {'+lines+'}';
 }
 
@@ -67,10 +107,7 @@ function parseObjective(xml) {
     return obj;
 }
 
-function parseMission(xml,index) {
-    var score = xml.score.map(parseScore);
-    var scoreKey = '@@@'+index+'@@@';
-    fnMap[scoreKey] = '['+score.join(',')+']';
+function parseMission(xml,index, resources) {
 
     //only return elements starting with 'objective-''
     var objectives = Object.keys(xml).filter(function(key) {
@@ -81,6 +118,15 @@ function parseMission(xml,index) {
             return objective;
         }));
     },[]);
+
+    var missionResources = objectives
+      .filter(obj => obj.$.resource)
+      .map(obj => resources[obj.$.resource])
+
+    var score = xml.score.map(s => parseScore(s, missionResources));
+    var scoreKey = '@@@'+index+'@@@';
+    fnMap[scoreKey] = '['+score.join(',')+']';
+
     return {
         title: getString(xml.$.name),
         description: getString(xml.$.description),
@@ -126,11 +172,14 @@ function processFile(fn) {
         strings = def['fll:challenge'].strings[0].string.reduce(parseString,{});
     // console.log(strings);
     // return;
+        var resources = parseResources(def['fll:challenge'])
+
         var missions = def['fll:challenge'].mission;
+
         // console.log(missions);
         var jsm = {
             title: def['fll:challenge'].$.name,
-            missions: missions.map(parseMission),
+            missions: missions.map((m, i) => parseMission(m, i, resources)),
             strings: strings,
             rtl: rtl
         };
@@ -142,8 +191,7 @@ function processFile(fn) {
 
         //beautify
         json = beautify(json, { indent_size: 4 });
-        console.log(json);
-    return;
+        console.log(`(${json})`);
 
         // var fn = 'missions/'+def.fll.season[0].$.year+'.js';
         // fs.writeFileSync(fn,json);
